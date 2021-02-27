39f6cf1735f708dac07341d074caa8481f5f761d
Jamie <jmaslek11@gmail.com>
2021-02-26 20:06:00 -0500

    Added final presentation nb.  Added binary classification models

--- scripts/generate_model.py
+++ scripts/generate_model.py
@@ -9,7 +9,13 @@ from sklearn.model_selection import train_test_split
 def gb_model(df, STATE_ID, n_to_keep = 10, drop_pct = 0.2, to_impute = True, scale = False , **kwargs):
 
     df = df[df.STATE == STATE_ID].reset_index(drop=True)
-    df = df.drop(columns = ['OBJECTID', 'OBJECTID_1','STATE'], inplace=False)
+    df = df.drop(columns = ['OBJECTID', 'OBJECTID_1','STATE',
+                            'COUNTY','GEOID','TRACT','NAME',
+                            'CNTY_FIPS', 'EACODE', 'EANAME',
+                            'GEOID_CHANGE', 'LIC', 'CROSS_STATE', 'SUBTRACTIONS',
+                            'ADDITIONS', 'TRACT_TYPE','FID','STUSAB',
+                            'Shape__Area','Shape__Length'
+                            ], inplace=False)
     df = df.loc[df.DESIGNATED.notnull()]
 
     drop_count = drop_pct * df.shape[0]
@@ -44,13 +50,19 @@ def gb_model(df, STATE_ID, n_to_keep = 10, drop_pct = 0.2, to_impute = True, sca
     clf.fit(X_train, y_train)
     cr = classification_report(y_test, clf.predict(X_test), output_dict=True)
     feat_importances = pd.Series(clf.feature_importances_, index=X.columns)
-    return feat_importances.nlargest(n_to_keep), clf.score(X_test, y_test), cr['2']['f1-score']
+    return feat_importances.nlargest(n_to_keep), clf.score(X_test, y_test), cr['2']['f1-score'], cr
 
 def gb_model_eligible_only(df, STATE_ID, n_to_keep = 10, drop_pct = 0.2, to_impute = True, scale = False , **kwargs):
 
     df = df[df.STATE == STATE_ID].reset_index(drop=True)
     df = df[df.DESIGNATED != 'NotEligible']
-    df = df.drop(columns = ['OBJECTID', 'OBJECTID_1','STATE'], inplace=False)
+    df = df.drop(columns = ['OBJECTID', 'OBJECTID_1', 'STATE',
+                            'COUNTY', 'GEOID', 'TRACT', 'NAME',
+                            'CNTY_FIPS', 'EACODE', 'EANAME',
+                            'GEOID_CHANGE', 'LIC', 'CROSS_STATE', 'SUBTRACTIONS',
+                            'ADDITIONS', 'TRACT_TYPE', 'FID', 'STUSAB',
+                            'Shape__Area', 'Shape__Length'
+                            ], inplace=False)
     df = df.loc[df.DESIGNATED.notnull()]
 
     drop_count = drop_pct * df.shape[0]
@@ -88,5 +100,54 @@ def gb_model_eligible_only(df, STATE_ID, n_to_keep = 10, drop_pct = 0.2, to_impu
     return feat_importances.nlargest(n_to_keep), clf.score(X_test, y_test), cr['1']['f1-score'],cr
 
 
+def cleaned_data_model(df, STATE_ID,n_to_keep = 10, drop_pct = 0.2, to_impute = True, scale = False , **kwargs):
+
+    df = df[df.STATE == STATE_ID].reset_index(drop=True)
+    df = df[df.DESIGNATED != 'NotEligible']
+    df = df.drop(columns=['OBJECTID', 'OBJECTID_1', 'STATE',
+                          'COUNTY', 'GEOID', 'TRACT', 'NAME',
+                          'CNTY_FIPS', 'EACODE', 'EANAME',
+                          'GEOID_CHANGE', 'LIC', 'CROSS_STATE', 'SUBTRACTIONS',
+                          'ADDITIONS', 'TRACT_TYPE', 'FID', 'STUSAB',
+                          'Shape__Area', 'Shape__Length'
+                          ], inplace=False)
+    df = df.loc[df.DESIGNATED.notnull()]
+
+    drop_count = drop_pct * df.shape[0]
+    null_cols = df.columns[df.isnull().sum() != 0]
+    many_null_cols = []
+    for col in null_cols:
+        count = df[col].isna().sum()
+        if count > drop_count:
+            many_null_cols.append(col)
+
+    df_clean = df.drop(columns=many_null_cols, inplace=False)
+    y = df_clean.pop('DESIGNATED')
+    X = df_clean.select_dtypes(np.number)
+    y.loc[y == False] = 'False'
+    y.loc[y == True] = 'True'
+    y_map = {'NotEligible': -1,
+             'False': 0,
+             'True': 1}
+    y = y.map(y_map)
+    cols = [c for c in X.columns if str(c).endswith('_PCT')]
+    drop_cols = [c[:-4] for c in cols if c[:-4] in X.columns]
+    X = X.drop(columns = drop_cols)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, random_state=2)
+
+    imp = SimpleImputer(strategy='mean')
+    ss = StandardScaler()
+    if to_impute:
+        X_train = imp.fit_transform(X_train)
+        X_test = imp.transform(X_test)
+    if scale:
+        X_train = ss.fit_transform(X_train)
+        X_test = ss.transform(X_test)
+
+    clf = GradientBoostingClassifier( **kwargs )
+    clf.fit(X_train, y_train)
+    cr = classification_report(y_test, clf.predict(X_test), output_dict=True)
+    feat_importances = pd.Series(clf.feature_importances_, index=X.columns)
+    return feat_importances.nlargest(n_to_keep), clf.score(X_test, y_test), cr['1']['f1-score'],cr
 
 
--- scripts/train_models.py
+++ scripts/train_models.py
@@ -4,6 +4,7 @@ import os, json, itertools
 from generate_model import *
 from collections import Counter
 import matplotlib.pyplot as plt
+import us
 
 #Load Premade dataframe that merges all 4 provided datasets
 df_merged = pd.read_pickle('../data/AllDataMerged.pkl')
@@ -15,13 +16,13 @@ with open('../data/variable_names.json', 'r') as fp:
     variable_translate = json.load(fp)
     #Define parameters for GradientBoostedClassfier
 
-model_args = {'learning_rate':.3, 'n_estimators':60,
-             'subsample':.8,'min_samples_split':3,
-             'min_samples_leaf':1,'max_depth':5,
-             'random_state':40,'max_features':'sqrt',
+model_args = {'learning_rate':.7, 'n_estimators':75,
+             'subsample':.8,'min_samples_split':2,
+             'min_samples_leaf':1,'max_depth':4,
+             'random_state':333,'max_features':'sqrt',
              'verbose':0,'max_leaf_nodes':None,
              'warm_start':False,'validation_fraction':0.1,
-             'n_iter_no_change':1 }
+             'n_iter_no_change':10 }
 
 
 # Generate a list of the 50 states + DC
@@ -29,6 +30,7 @@ state_list = [i for i in range(1,57)]
 drop_list = [3,7,14,43,52]
 
 for drop in drop_list:
+
     state_list.remove(drop)
 
 # Model returns a list of n_to_keep most important features,
@@ -37,14 +39,17 @@ feat_dict = {}
 scores = []
 f1 = []
 features =[]
+state_names = []
 for i in state_list:
 
-    a,b,c,d = gb_model_eligible_only(df_merged, i, n_to_keep=10 , **model_args)
+    #a,b,c,d = gb_model(df_merged, i, n_to_keep=10 , **model_args)
+    a,b,c,d = cleaned_data_model(df_merged, i, n_to_keep=10 , **model_args)
 
     feat_dict[i] = a
     features.append(a.index.to_list())
     scores.append(b)
     f1.append(c)
+    state_names.append(us.states.lookup(str(i).zfill(2)).abbr)
 
 # Flatten feature list then count what has the most
 
